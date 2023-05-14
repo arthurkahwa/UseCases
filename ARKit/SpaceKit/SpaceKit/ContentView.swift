f72737540a8051fd40a1186319d058f2012ed0ff
Arthur Kahwa <arthur_kahwa@chapter.de>
2023-05-14 20:16:05 +0200

    Update coordinator class

--- ARKit/SpaceKit/SpaceKit/ContentView.swift
+++ ARKit/SpaceKit/SpaceKit/ContentView.swift
@@ -39,13 +39,12 @@ struct ContentView : View {
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    var kitName: String
+    var kitName: String?
     
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
         context.coordinator.arView = arView
-        context.coordinator.kitName = kitName
         
         let tapGestureRecogniser = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap(from:)))
         arView.addGestureRecognizer(tapGestureRecogniser)
@@ -54,53 +53,56 @@ struct ARViewContainer: UIViewRepresentable {
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(self)
     }
-
+    
     func updateUIView(_ uiView: ARView, context: Context) {}
     
-}
-
-class Coordinator: NSObject {
-    weak var arView: ARView?
-    var kitName: String?
-    var cancellable: AnyCancellable?
-    
-    @objc
-    func handleTap(from recogniser: UIGestureRecognizer) {
-        // Make sure we have a view to work with
-        guard let arView = arView
-        else { return }
+    class Coordinator: NSObject {
+        var parent: ARViewContainer
         
-        // Make sure we do not have duplicates
-        guard arView.scene.anchors.first(where: { $0.name == kitName}) == nil
-        else {
-            return
+        weak var arView: ARView?
+        var cancellable: AnyCancellable?
+        
+        init(_ parent: ARViewContainer) {
+            self.parent = parent
         }
-
-        // Make sure we have the name of kit we want to show
-        if let kitName = kitName {
-            if let raycast = arView.raycast(from: arView.center,
-                                            allowing: .estimatedPlane,
-                                            alignment: .horizontal).first {
-                let anchor = AnchorEntity(raycastResult: raycast)
-                anchor.name = kitName
-                
-                cancellable = ModelEntity.loadAsync(named: kitName)
-                    .sink(receiveCompletion: { fetchComplete in
-                        if case let .failure(error) = fetchComplete {
-                            print("Error: \(String(describing: error))")
-                        }
-                        
-                        self.cancellable?.cancel()
-                        
-                    }, receiveValue: { modelEntity in
-                        anchor.addChild(modelEntity)
-                    })
-                
-                arView.scene.addAnchor(anchor)
+        
+        @objc
+        func handleTap(from recogniser: UIGestureRecognizer) {
+            // Make sure we have a view to work with
+            guard let arView = arView
+            else { return }
+            
+            // Make sure we do not have duplicates
+            guard arView.scene.anchors.first(where: { $0.name == parent.kitName}) == nil
+            else {
+                return
+            }
+            
+            // Make sure we have the name of kit we want to show
+            if let kitName = parent.kitName {
+                if let raycast = arView.raycast(from: arView.center,
+                                                allowing: .estimatedPlane,
+                                                alignment: .horizontal).first {
+                    let anchor = AnchorEntity(raycastResult: raycast)
+                    anchor.name = kitName
+                    
+                    cancellable = ModelEntity.loadAsync(named: kitName)
+                        .sink(receiveCompletion: { fetchComplete in
+                            if case let .failure(error) = fetchComplete {
+                                print("Error: \(String(describing: error))")
+                            }
+                            
+                            self.cancellable?.cancel()
+                            
+                        }, receiveValue: { modelEntity in
+                            anchor.addChild(modelEntity)
+                        })
+                    
+                    arView.scene.addAnchor(anchor)
+                }
             }
         }
     }
 }
-
