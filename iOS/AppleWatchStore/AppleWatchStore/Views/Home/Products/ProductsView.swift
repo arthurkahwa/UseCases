311fe028a7433ecff2063e2f13c24c3ce1180fcf
Arthur Kahwa <arthur_kahwa@chapter.de>
2023-10-08 17:25:20 +0200

    Design pr

--- iOS/AppleWatchStore/AppleWatchStore/Views/Home/Products/ProductsView.swift
+++ iOS/AppleWatchStore/AppleWatchStore/Views/Home/Products/ProductsView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ProductsView: View {
     var body: some View {
-        Text("Hello Products")
+        NavigationStack {
+            ScrollView {
+                content
+            }
+            .navigationTitle("APPLE WATCHES")
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+    
+    var content: some View {
+        ProductsGridView()
     }
 }
 
--- iOS/AppleWatchStore/AppleWatchStore/Views/Home/Products/SupportingViews/ProductsGridView.swift
+++ iOS/AppleWatchStore/AppleWatchStore/Views/Home/Products/SupportingViews/ProductsGridView.swift
@@ -9,19 +9,17 @@ import SwiftUI
 
 struct ProductsGridView: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                LazyVGrid(columns: Constants.columns, spacing: 16) {
-                    ForEach(0 ..< 12) { _ in
-                        NavigationLink {} label: {
-                            GridProductItem()
-                        }
-                        .buttonStyle(.plain)
+        VStack {
+            LazyVGrid(columns: Constants.columns, spacing: 16) {
+                ForEach(0 ..< 12) { _ in
+                    NavigationLink {} label: {
+                        GridProductItem()
                     }
+                    .buttonStyle(.plain)
                 }
             }
-            .padding(.top, 2)
         }
+        .padding(.top, 2)
     }
 }
 
